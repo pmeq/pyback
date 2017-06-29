2c2b845398ebd937ed488aed572f5af1106842b1
sunwenhui <sunwenhui@jd.com>
2017-06-29 17:31:55 +0800

    1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 from distutils.core import setup
+from distutils.command.install import install
+
+script_file = 'script/pyback'
+
+
+class MyInstall(install):
+    def run(self):
+        self.change_path()
+        install.run(self)
+
+    def change_path(self):
+        python_executable = sys.executable
+
+        # change the python file path
+        with open(script_file, 'r+') as fh:
+            d = fh.read().replace('/usr/bin/env python', python_executable)
+            fh.seek(0)
+            fh.truncate()
+            fh.write(d)
 
 setup(
     name='pyback',
@@ -16,12 +36,15 @@ setup(
         'pyback.PyBack'
     ],
     scripts=[
-        "script/pyback",
+        script_file,
     ],
     install_requires=[
         'pyhdfs'
     ],
     data_files=[
         ('/export/servers/conf', ['config/hdfs.cfg', ]),
-    ]
+    ],
+    cmdclass={
+        'install': MyInstall,
+    },
 )
